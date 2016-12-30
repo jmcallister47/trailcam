e4a07628487ec46e96c1e056b1dd913b91f0bcd8
Joseph McAllister <joseph.l.mcallister@gmail.com>
2016-12-30 13:39:34 -0600

    Change night time motion turnOnLights() and turnOfLights() to work with new IR lights

--- night/motion-camera.py
+++ night/motion-camera.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 '''Script that manages motion sensor, camera module and light
    When motion is detected, turn on infrared light, take and save picture, turn off light
-   Maximum of one picture every 5 seconds'''
+   Maximum of one picture every 4 seconds'''
 from gpiozero import MotionSensor
 import subprocess
 from datetime import datetime
 from time import sleep
+import RPi.GPIO as GPIO
 
 def main():
     sensor = MotionSensor(4)
     writeToLogFile("STARTED NIGHT MODE AT " + str(datetime.now()))
+    GPIO.setmode(GPIO.BCM) 
+    GPIO.setup(5, GPIO.OUT) #setup light trigger
     while True:
         if sensor.motion_detected:
             turnOnLights()
+            print "Lights on"
+            sleep(1)
             takePicture() 
-            sleep(2) 
+            print("Take picture")
+            #sleep(3) 
             turnOffLights()
-            sleep(3) 
+            print("Turn off lights")
+            sleep(4) 
             writeToLogFile("Took one picture at " + str(datetime.now()))
 
-'''Turns on IR Lights indefinetely'''     
+'''Turns on IR Lights indefinetely'''
 def turnOnLights():
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x00", "0xa5"]) #turn on IR lights    
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x09", "0x0f"]) #turn gain to full
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x01", "0x32"]) #full brightness
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x03", "0x32"])
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x06", "0x32"])
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x08", "0x32"]) 
-
+    GPIO.output(5, 0)
+    
 '''Turns off all lights (IR and LED)'''
 def turnOffLights():
-    subprocess.call(["sudo", "i2cset", "-y", "1", "0x70", "0x00", "0x00"])
+    GPIO.output(5, 1)
 
 '''Takes a picture and saves it with timestamp'''
 def takePicture():
